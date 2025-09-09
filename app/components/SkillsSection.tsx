4fb675172534bb1d951d043e4d1fbc9151332b5d
Alex Domnikov <179528890+alexdomnikov@users.noreply.github.com>
2025-09-09 13:46:45 -0700

    Splitting up sections, updating known tech

--- app/components/SkillsSection.tsx
+++ app/components/SkillsSection.tsx
@@ -1,21 +1,34 @@
 export default function SkillsSection() {
-	const skills = ['JavaScript', 'TypeScript', 'React', 'Node.js', 'Next.js', 'TailwindCSS', 'PostgreSQL', 'GraphQL', 'Git'];
+	const languages = ['C++', 'Python', 'Java', 'HTML', 'CSS', 'Javascript', 'SQL', 'GraphQL']
+	const tools = ['React', 'Node.js', 'Next.js', 'TailwindCSS', 'PostgreSQL', 'Git'];
 
 	return (
 		<section className="bg-gray-800 py-12 sm:py-20">
 			<div className="max-w-4xl mx-auto px-4 sm:px-6 lg:px-8">
-				<h2 className="text-xl sm:text-2xl font-bold text-white mb-6 sm:mb-8 text-center">Skills & Technologies</h2>
+				<h2 className="text-xl sm:text-2xl font-bold text-white mb-6 sm:mb-8 text-center">Languages</h2>
 				<div className="flex flex-wrap justify-center gap-4">
-					{skills.map((skill) => (
+					{languages.map((language) => (
 						<div
-							key={skill}
+							key={language}
 							className="bg-gray-700 p-4 sm:p-6 rounded-lg shadow-sm text-center hover:shadow-md transition-shadow hover:shadow-gray-500 text-sm sm:text-base text-gray-200"
 						>
-							{skill}
+							{language}
 						</div>
 					))}
 				</div>
 			</div>
+
+		<h2 className="text-xl sm:text-2xl font-bold text-white mb-6 sm:mb-8 text-center">Libraries, Frameworks, and Tools</h2>
+				<div className="flex flex-wrap justify-center gap-4">
+					{tools.map((tool) => (
+						<div
+							key={tool}
+							className="bg-gray-700 p-4 sm:p-6 rounded-lg shadow-sm text-center hover:shadow-md transition-shadow hover:shadow-gray-500 text-sm sm:text-base text-gray-200"
+						>
+							{tool}
+						</div>
+					))}
+				</div>
 		</section>
 	);
 }
