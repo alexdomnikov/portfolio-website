fb6b9d5046aefa07b726695fa1a914a9ec4140b7
Alex Domnikov <179528890+alexdomnikov@users.noreply.github.com>
2025-09-09 15:13:31 -0700

    Fixing margins

--- app/components/HeroSection.tsx
+++ app/components/HeroSection.tsx
@@ -10,7 +10,7 @@ export default function HeroSection() {
 					<Image src="/portrait.jpg" alt="Profile Picture" width={120} height={120} className="rounded-full mx-auto border-4 border-gray-700 shadow-lg" />
 				</div>
 				<h1 className="text-3xl sm:text-4xl font-bold text-white mb-3 sm:mb-4">Alex Domnikov</h1>
-				<p className="text-lg sm:text-xl text-gray-300 mb-6 sm:mb-8">MSCS Candidate at Georgia Tech | Specializing in Computing Systems & Applied AI/ML.</p>
+				<p className="text-lg sm:text-xl text-gray-300 mb-1 sm:mb-2">MSCS Candidate at Georgia Tech | Specializing in Computing Systems & Applied AI/ML.</p>
 				<p className="text-gray-400 max-w-2xl mx-auto text-sm sm:text-base px-4">
 					Former finance professional turned systems-focused engineer. Passionate about crafting elegant, AI-powered solutions to real-world business challenges.
 				</p>
